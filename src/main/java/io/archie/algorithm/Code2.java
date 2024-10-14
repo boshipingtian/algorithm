d68e29e17419b80b0f3622870b4433011581abf6
MrArchie <1619794359@qq.com>
2024-10-14 18:52:41 +0800

    new code

---
+++ src/main/java/io/archie/algorithm/Code2.java
@@ -0,0 +1,14 @@
+package io.archie.algorithm;
+
+/**
+ * <br>
+ *
+ * @Author: MrArchie
+ * @Date: 2024/10/14 18:52
+ */
+public class Code2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
