e205e7cf9e4a696a65793eb27d1aba60597ff50e
MrArchie <1619794359@qq.com>
2024-10-15 09:02:52 +0800

    华为技术二面-合法括号

--- src/main/java/io/archie/algorithm/Code2.java
+++ src/main/java/io/archie/algorithm/Code2.java
@@ -1,5 +1,7 @@
 package io.archie.algorithm;
 
+import java.util.Stack;
+
 /**
  * <br>
  *
@@ -9,6 +11,40 @@ package io.archie.algorithm;
 public class Code2 {
 
     public static void main(String[] args) {
-        
+        String test1 = "()";
+        String test2 = "()[]{}";
+        String test3 = "(]";
+        String test4 = "([)]";
+        String test5 = "{[]}";
+        System.out.println(checkSqua(test1));
+        System.out.println(checkSqua(test2));
+        System.out.println(checkSqua(test3));
+        System.out.println(checkSqua(test4));
+        System.out.println(checkSqua(test5));
+        System.out.println(checkSqua("(("));
     }
+    
+    public static boolean checkSqua(String s){
+        if("".equals(s)){
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()){
+                stack.push(c);
+                continue;
+            }
+            if(')' == c && '(' == stack.peek()){
+                stack.pop();
+            }else if(']' == c && '[' == stack.peek()){
+                stack.pop();
+            }else if('}' == c && '{' == stack.peek()){
+                stack.pop();
+            }else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+    
 }
